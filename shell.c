401f871703216ea56e799a57d77a09513280291c
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-04-23 21:48:00 +0200

    empty application

---
+++ shell.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * main - A function that runs our shell.
+ * @argc: The number of inputed arguments.
+ * @argv: The pointer to array of inputed arguments.
+ * @envp: The pointer to array of enviromental variables.
+ * Return: Always 0.
+ */
+int main(__attribute__((unused)) int argc, char **argv, char **envp)
+{
+	return (EXIT_SUCCESS);
+}
---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+#endif /* _SHELL_H_ */
